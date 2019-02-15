c91a74dfaf40fa6714469483a4ffe516e0395947
erdakjo <ekenney60@gmail.com>
2019-02-15 15:30:10 -0700

    burger app final files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Barbecue Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Ultimat Mega Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ultimate Mega Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Bacon Ranch', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
