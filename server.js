f184c84b73efa667e1424e372cb8bb1ed262186b
erdakjo <ekenney60@gmail.com>
2019-02-16 18:24:56 -0700

    updating styles and favicon

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require("express");
+ var favicon = require('serve-favicon')
+ var path = require('path')
+
+
 
 var PORT = process.env.PORT || 8080;
 var app = express();
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +22,6 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("Listening on port:%s", PORT);
 });
